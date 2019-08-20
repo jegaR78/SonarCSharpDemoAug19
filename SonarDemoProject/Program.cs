e3c48cb92b213211b915cf64c0fed0babe89ec7a
jegaR78 <54302552+jegaR78@users.noreply.github.com>
2019-08-20 10:57:50 +0530

    Initial version

---
+++ SonarDemoProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarDemoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string empName;
+        }
+        private void empProfile()
+        {
+
+        }
+    }
+}
