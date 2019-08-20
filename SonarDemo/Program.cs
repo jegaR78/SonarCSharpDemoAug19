f4172a0499e552663c8db64aa1aaaf07d23894e4
jegaR78 <54302552+jegaR78@users.noreply.github.com>
2019-08-20 11:43:50 +0530

    by sonarManualSetup

---
+++ SonarDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int _empID = 0;
+            string _empName = string.Empty;
+
+        }
+    }
+}
